66c315d98ed3d5be605c4d5d7161556cea2d5c3c
norman6464 <175028044+norman6464@users.noreply.github.com>
2026-01-08 15:29:46 +0900

    Spring bootのサービスの依存関係修正

--- follow/src/test/java/com/example/follow/FollowApplicationTests.java
+++ follow/src/test/java/com/example/follow/FollowApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.chat;
+package com.example.follow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChatApplicationTests {
+class FollowApplicationTests {
 
 	@Test
 	void contextLoads() {
